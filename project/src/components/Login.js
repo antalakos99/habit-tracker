488f69e0f06c8db72eb0ec14722c99a27d0a0bce
AntalAkos <akos.antal50@gmail.com>
2022-02-12 16:14:02 +0200

    Kezdetleges login és register komponensek

--- project/src/components/Login.js
+++ project/src/components/Login.js
@@ -1,11 +1,88 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { TextField } from "@mui/material";
+import { Button } from "@mui/material";
+import { connect } from "react-redux";
+import { createUser } from "../redux";
+import { useHistory } from "react-router-dom";
+import { createGlobalState } from "react-hooks-global-state";
 
-function Login() {
+const { setGlobalState, useGlobalState } = createGlobalState({
+  authorized: false,
+});
+
+function Login({ user }) {
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [password, setPassword] = useState("");
+  let history = useHistory();
+  useEffect(() => {
+    console.log();
+  }, []);
+  const handleSubmit = (fname, lname, password) => {
+    if (
+      fname === user.firstName &&
+      lname === user.lastName &&
+      password === user.password
+    ) {
+      history.push("/habittracker");
+      setGlobalState("authorized", true);
+    } else {
+      alert("not Aight");
+      console.log(fname, lname, password);
+      console.log(user.firstName, user.lastName, user.password);
+    }
+  };
   return (
-    <div>
-      <h2>Login</h2>
-    </div>
+    <form>
+      <TextField
+        id="fname"
+        label="Firstname"
+        variant="filled"
+        margin="normal"
+        value={fname}
+        onChange={(e) => setFname(e.target.value)}
+      />
+      <TextField
+        id="lname"
+        label="Lastname"
+        variant="filled"
+        margin="normal"
+        value={lname}
+        onChange={(e) => setLname(e.target.value)}
+      />
+      <TextField
+        id="password"
+        label="Password"
+        type="password"
+        autoComplete="current-password"
+        variant="filled"
+        margin="normal"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button
+        variant="contained"
+        size="normal"
+        onClick={() => handleSubmit(fname, lname, password)}
+      >
+        Login
+      </Button>
+    </form>
   );
 }
 
-export default Login;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    makeUser: (firstName, lastName, password, age, profilePicture) =>
+      dispatch(createUser(firstName, lastName, password, age, profilePicture)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export { useGlobalState, setGlobalState };
--- project/src/components/Register.js
+++ project/src/components/Register.js
@@ -1,11 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
+import { TextField } from "@mui/material";
+import { Button } from "@mui/material";
+import { connect } from "react-redux";
+import { createUser } from "../redux";
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
-function Register() {
+function Register({ makeUser }) {
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [password, setPassword] = useState("");
+  const [age, setAge] = useState("");
+  const [ppic, setPpic] = useState("");
+  let history = useHistory();
+  const handleSubmit = () => {
+    makeUser(fname, lname, password, age, ppic);
+    history.push("/");
+  };
   return (
-    <div>
-      <h2>Register</h2>
-    </div>
+    <form>
+      <TextField
+        id="fname"
+        label="Firstname"
+        variant="filled"
+        margin="normal"
+        value={fname}
+        onChange={(e) => setFname(e.target.value)}
+      />
+      <TextField
+        id="lname"
+        label="Lastname"
+        variant="filled"
+        margin="normal"
+        value={lname}
+        onChange={(e) => setLname(e.target.value)}
+      />
+      <TextField
+        id="password"
+        label="Password"
+        type="password"
+        autoComplete="current-password"
+        variant="filled"
+        margin="normal"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <TextField
+        id="age"
+        label="Age"
+        type="number"
+        variant="filled"
+        margin="normal"
+        value={age}
+        onChange={(e) => setAge(e.target.value)}
+      />
+      <TextField
+        id="ppic"
+        label="Profile picture (paste the url)"
+        variant="filled"
+        margin="normal"
+        value={ppic}
+        onChange={(e) => setPpic(e.target.value)}
+      />
+      <Button variant="contained" size="normal" onClick={handleSubmit}>
+        Register
+      </Button>
+      <Link to="/">Login</Link>
+    </form>
   );
 }
 
-export default Register;
+const mapStateToProps = (state) => {
+  return {
+    fname: state.user.firstName,
+    lname: state.user.lastName,
+    age: state.user.age,
+    proPic: state.user.profilePicture,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    makeUser: (firstName, lastName, password, age, profilePicture) =>
+      dispatch(createUser(firstName, lastName, password, age, profilePicture)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Register);
