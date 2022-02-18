770021ae4a831e60f9007d613e70567c3772d0cf
AntalAkos <akos.antal50@gmail.com>
2022-02-18 17:38:23 +0200

    Regisztrálas, belépés json serverrel

--- project/src/components/Login.js
+++ project/src/components/Login.js
@@ -1,54 +1,40 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { TextField } from "@mui/material";
 import { Button } from "@mui/material";
-import { connect } from "react-redux";
-import { createUser } from "../redux";
 import { useHistory } from "react-router-dom";
 import { createGlobalState } from "react-hooks-global-state";
+import { useDispatch } from "react-redux";
+import { login } from "../redux";
+import axios from "axios";
 
 const { setGlobalState, useGlobalState } = createGlobalState({
   authorized: false,
 });
-
-function Login({ user }) {
-  const [fname, setFname] = useState("");
-  const [lname, setLname] = useState("");
+function Login() {
+  const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   let history = useHistory();
-  useEffect(() => {
-    console.log();
-  }, []);
-  const handleSubmit = (fname, lname, password) => {
-    if (
-      fname === user.firstName &&
-      lname === user.lastName &&
-      password === user.password
-    ) {
-      history.push("/habittracker");
-      setGlobalState("authorized", true);
-    } else {
-      alert("not Aight");
-      console.log(fname, lname, password);
-      console.log(user.firstName, user.lastName, user.password);
-    }
+  const dispatch = useDispatch();
+  const handleSubmit = (name, password) => {
+    axios.get("http://localhost:3004/users").then((res) => {
+      res.data.map((account) => {
+        if (name === account.name && password === account.password) {
+          dispatch(login(account.id));
+          history.push("/habittracker");
+          setGlobalState("authorized", true);
+        }
+      });
+    });
   };
   return (
     <form>
       <TextField
-        id="fname"
+        id="name"
         label="Firstname"
         variant="filled"
         margin="normal"
-        value={fname}
-        onChange={(e) => setFname(e.target.value)}
-      />
-      <TextField
-        id="lname"
-        label="Lastname"
-        variant="filled"
-        margin="normal"
-        value={lname}
-        onChange={(e) => setLname(e.target.value)}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
       />
       <TextField
         id="password"
@@ -63,7 +49,7 @@ function Login({ user }) {
       <Button
         variant="contained"
         size="normal"
-        onClick={() => handleSubmit(fname, lname, password)}
+        onClick={() => handleSubmit(name, password)}
       >
         Login
       </Button>
@@ -71,18 +57,5 @@ function Login({ user }) {
   );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    user: state.user,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    makeUser: (firstName, lastName, password, age, profilePicture) =>
-      dispatch(createUser(firstName, lastName, password, age, profilePicture)),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default Login;
 export { useGlobalState, setGlobalState };
--- project/src/components/Register.js
+++ project/src/components/Register.js
@@ -2,38 +2,30 @@ import React, { useState } from "react";
 import { TextField } from "@mui/material";
 import { Button } from "@mui/material";
 import { connect } from "react-redux";
-import { createUser } from "../redux";
-import { Link } from "react-router-dom";
+import { setAccount } from "../redux";
 import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { registration } from "../redux";
 
-function Register({ makeUser }) {
-  const [fname, setFname] = useState("");
-  const [lname, setLname] = useState("");
+function Register({ setAccount }) {
+  const [name, setName] = useState("");
   const [password, setPassword] = useState("");
-  const [age, setAge] = useState("");
-  const [ppic, setPpic] = useState("");
+  const dispatch = useDispatch();
   let history = useHistory();
   const handleSubmit = () => {
-    makeUser(fname, lname, password, age, ppic);
+    setAccount(name, password);
+    dispatch(registration());
     history.push("/");
   };
   return (
     <form>
       <TextField
-        id="fname"
-        label="Firstname"
+        id="name"
+        label="name"
         variant="filled"
         margin="normal"
-        value={fname}
-        onChange={(e) => setFname(e.target.value)}
-      />
-      <TextField
-        id="lname"
-        label="Lastname"
-        variant="filled"
-        margin="normal"
-        value={lname}
-        onChange={(e) => setLname(e.target.value)}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
       />
       <TextField
         id="password"
@@ -45,44 +37,23 @@ function Register({ makeUser }) {
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
-      <TextField
-        id="age"
-        label="Age"
-        type="number"
-        variant="filled"
-        margin="normal"
-        value={age}
-        onChange={(e) => setAge(e.target.value)}
-      />
-      <TextField
-        id="ppic"
-        label="Profile picture (paste the url)"
-        variant="filled"
-        margin="normal"
-        value={ppic}
-        onChange={(e) => setPpic(e.target.value)}
-      />
       <Button variant="contained" size="normal" onClick={handleSubmit}>
         Register
       </Button>
-      <Link to="/">Login</Link>
     </form>
   );
 }
 
 const mapStateToProps = (state) => {
   return {
-    fname: state.user.firstName,
-    lname: state.user.lastName,
-    age: state.user.age,
-    proPic: state.user.profilePicture,
+    name: state.account.name,
+    password: state.account.password,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    makeUser: (firstName, lastName, password, age, profilePicture) =>
-      dispatch(createUser(firstName, lastName, password, age, profilePicture)),
+    setAccount: (name, password) => dispatch(setAccount(name, password)),
   };
 };
 
