b4abcf09012be67d6cc0da795d6bc6247a34d52a
AntalAkos <akos.antal50@gmail.com>
2022-02-23 14:02:29 +0200

    Belépés-regisztrálás validálás változása

--- project/src/components/Login.js
+++ project/src/components/Login.js
@@ -1,59 +1,126 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { TextField } from "@mui/material";
 import { Button } from "@mui/material";
 import { useHistory } from "react-router-dom";
 import { createGlobalState } from "react-hooks-global-state";
 import { useDispatch } from "react-redux";
 import { login } from "../redux";
+import { Paper } from "@mui/material";
+import { getAccounts, getNames } from "../redux";
 import axios from "axios";
 
 const { setGlobalState, useGlobalState } = createGlobalState({
   authorized: false,
 });
 function Login() {
-  const [name, setName] = useState("");
+  const [id, setId] = useState("");
+  const [flag, setFlag] = useState(false);
+  const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
   let history = useHistory();
   const dispatch = useDispatch();
-  const handleSubmit = (name, password) => {
-    axios.get("http://localhost:3004/users").then((res) => {
-      res.data.map((account) => {
-        if (name === account.name && password === account.password) {
-          dispatch(login(account.id));
-          history.push("/habittracker");
-          setGlobalState("authorized", true);
-        }
-      });
-    });
+  const names = dispatch(getNames());
+  const accounts = dispatch(getAccounts());
+  const handleSubmit = () => {
+    setFormErrors(validate(userName, password));
+    setIsSubmit(true);
   };
+  const validate = (userName, password) => {
+    const errors = {};
+    accounts.map((account) => {
+      if (account.name === userName && account.password === password) {
+        setFlag(true);
+        setId(account.id);
+      }
+    });
+    accounts.map((account) => {
+      if (!flag && account.name !== userName) {
+        errors.userName = "The given name doesn`t exist!";
+      }
+    });
+    accounts.map((account) => {
+      if (!flag && account.name === userName && account.password !== password) {
+        errors.password = "Incorrect password!";
+      }
+    });
+    return errors;
+  };
+
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit && flag) {
+      dispatch(login(id));
+      history.push("/habittracker");
+    }
+  }, [formErrors]);
   return (
-    <form>
-      <TextField
-        id="name"
-        label="Firstname"
-        variant="filled"
-        margin="normal"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <TextField
-        id="password"
-        label="Password"
-        type="password"
-        autoComplete="current-password"
-        variant="filled"
-        margin="normal"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <Button
-        variant="contained"
-        size="normal"
-        onClick={() => handleSubmit(name, password)}
-      >
-        Login
-      </Button>
-    </form>
+    <Paper elevation={2} className="wrapper">
+      <div className="formBox">
+        {!formErrors.userName ? (
+          <TextField
+            id="nameInput"
+            label="Name"
+            variant="filled"
+            margin="normal"
+            fullWidth
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        ) : (
+          <TextField
+            error
+            id="nameInput"
+            label="Name"
+            variant="filled"
+            margin="normal"
+            fullWidth
+            value={userName}
+            helperText={formErrors.userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        )}
+        {!formErrors.password ? (
+          <TextField
+            id="passwordInput"
+            label="Password"
+            type="password"
+            variant="filled"
+            margin="normal"
+            fullWidth
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        ) : (
+          <TextField
+            error
+            id="passwordInput"
+            label="Password"
+            type="password"
+            variant="filled"
+            margin="normal"
+            fullWidth
+            value={password}
+            helperText={formErrors.password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        )}
+        <Button variant="contained" size="large" onClick={() => handleSubmit()}>
+          Login
+        </Button>
+        <Button
+          className="right"
+          variant="contained"
+          size="large"
+          onClick={() => {
+            history.push("/register");
+          }}
+        >
+          Sign up
+        </Button>
+      </div>
+    </Paper>
   );
 }
 
--- project/src/components/Register.js
+++ project/src/components/Register.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { TextField } from "@mui/material";
 import { Button } from "@mui/material";
 import { connect } from "react-redux";
@@ -6,41 +6,115 @@ import { setAccount } from "../redux";
 import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { registration } from "../redux";
+import { Paper } from "@mui/material";
+import { getNames } from "../redux";
 
 function Register({ setAccount }) {
-  const [name, setName] = useState("");
+  const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
   const dispatch = useDispatch();
-  let history = useHistory();
+  const history = useHistory();
   const handleSubmit = () => {
-    setAccount(name, password);
-    dispatch(registration());
-    history.push("/");
+    setFormErrors(validate(userName, password));
+    setIsSubmit(true);
   };
+  const names = dispatch(getNames());
+  const validate = (name, password) => {
+    const errors = {};
+    names.map((dbname) => {
+      if (dbname === name) {
+        errors.username = "Username is taken";
+      }
+    });
+    if (!name) {
+      errors.username = "Username is required";
+    }
+    if (!password) {
+      errors.password = "Password is required";
+    } else if (password.length < 5) {
+      errors.password = "Your password must be longer than 5 characters";
+    }
+    return errors;
+  };
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      setAccount(userName, password);
+      dispatch(registration());
+      history.push("/");
+    }
+  }, [formErrors]);
   return (
-    <form>
-      <TextField
-        id="name"
-        label="name"
-        variant="filled"
-        margin="normal"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <TextField
-        id="password"
-        label="Password"
-        type="password"
-        autoComplete="current-password"
-        variant="filled"
-        margin="normal"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <Button variant="contained" size="normal" onClick={handleSubmit}>
-        Register
-      </Button>
-    </form>
+    <Paper elevation={2} className="wrapper">
+      <div className="formBox">
+        {!formErrors.username ? (
+          <TextField
+            id="nameInput"
+            label="Name"
+            variant="filled"
+            margin="normal"
+            value={userName}
+            onChange={(e) => {
+              setUserName(e.target.value);
+            }}
+            fullWidth
+          />
+        ) : (
+          <TextField
+            error
+            id="nameInput"
+            label="Name"
+            variant="filled"
+            margin="normal"
+            value={userName}
+            onChange={(e) => {
+              setUserName(e.target.value);
+            }}
+            helperText={formErrors.username}
+            fullWidth
+          />
+        )}
+        {!formErrors.password ? (
+          <TextField
+            id="passwordInput"
+            label="Password"
+            type="password"
+            variant="filled"
+            margin="normal"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            fullWidth
+          />
+        ) : (
+          <TextField
+            error
+            id="passwordInput"
+            label="Password"
+            type="password"
+            variant="filled"
+            margin="normal"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            helperText={formErrors.password}
+            fullWidth
+          />
+        )}
+        <Button
+          className="center"
+          variant="contained"
+          size="Large"
+          onClick={() => handleSubmit()}
+        >
+          Register
+        </Button>
+      </div>
+    </Paper>
   );
 }
 
