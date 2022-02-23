9be1abca030b049d087dfbc2183ed2696cc417fb
AntalAkos <akos.antal50@gmail.com>
2022-02-23 14:01:00 +0200

    Design

--- project/src/components/SideBar.js
+++ project/src/components/SideBar.js
@@ -1,9 +1,14 @@
-import { List, ListItemButton, ListItemText } from "@mui/material";
-import React from "react";
+import { Box, List, ListItemButton, ListItemText } from "@mui/material";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/Menu";
+import useWindowDimensions from "../getWindowDimensions";
 
 function SideBar() {
   const history = useHistory();
+  const { width, height } = useWindowDimensions();
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
   const goToProfile = () => {
     history.push("/userprofile");
   };
@@ -13,28 +18,58 @@ function SideBar() {
   const goToHabitTracker = () => {
     history.push("/habittracker");
   };
+  useEffect(() => {
+    if (width < 1200 && sidebar === false) {
+      setSidebar(true);
+    }
+  }, [width]);
+
   return (
-    <div className="sidebar">
-      <List>
-        <ListItemButton onClick={goToProfile}>
-          <ListItemText
-            primary="Your profile"
-            secondary="Set your information here"
-          ></ListItemText>
-        </ListItemButton>
-        <ListItemButton onClick={goToHabits}>
-          <ListItemText
-            primary="Your habits"
-            secondary="Manage your habits here"
-          ></ListItemText>
-        </ListItemButton>
-        <ListItemButton onClick={goToHabitTracker}>
-          <ListItemText
-            primary="Habit tracker"
-            secondary="Manage your progress here"
-          ></ListItemText>
-        </ListItemButton>
-      </List>
+    <div className={sidebar ? "side-bar" : "side-bar active"}>
+      <MenuIcon
+        fontSize="large"
+        onClick={() => showSidebar()}
+        sx={{
+          float: "right",
+          marginTop: "1rem",
+          marginRight: "1.5rem",
+          ":hover": { cursor: "pointer", color: "white" },
+        }}
+      />
+      <Box sx={{ position: "relative", top: "10rem" }}>
+        <List>
+          <ListItemButton onClick={goToProfile}>
+            <ListItemText
+              className="sideItem"
+              primary="Your profile"
+              secondary="Set your information here"
+              primaryTypographyProps={{
+                fontSize: "larger",
+              }}
+            ></ListItemText>
+          </ListItemButton>
+          <ListItemButton onClick={goToHabits}>
+            <ListItemText
+              className="sideItem"
+              primary="Your habits"
+              secondary="Manage your habits here"
+              primaryTypographyProps={{
+                fontSize: "larger",
+              }}
+            ></ListItemText>
+          </ListItemButton>
+          <ListItemButton onClick={goToHabitTracker}>
+            <ListItemText
+              className="sideItem"
+              primary="Habit tracker"
+              secondary="Manage your progress here"
+              primaryTypographyProps={{
+                fontSize: "larger",
+              }}
+            ></ListItemText>
+          </ListItemButton>
+        </List>
+      </Box>
     </div>
   );
 }
